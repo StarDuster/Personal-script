7f448e3c4b5b6f7cd149dfa12450f15a87db598d
StarDuster <mail@starduster.me>
2018-08-02 01:42:07 +0800

    add fuzzyssh

---
+++ fuzzyssh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Usage: fuzzyssh.sh keyword
+# search keyword reversely in ~/.ssh
+# display all matchs and choose one to connect
+
+PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
+userdir=~
+declare -a hosts
+alias grep="grep --exclude=$userdir/.ssh/known_hosts 
+            --exclude=$userdir/.ssh/\*id
+            --exclude=$userdir/.ssh/multiplex
+            --exclude=$userdir/.ssh/authorized_keys"
+hosts=($(grep -r $1 ~/.ssh | awk '{ print $2}'))
+if [ -n "$hosts" ]; then 
+    for i in "${!hosts[@]}"; do
+        printf "%s\t%s\n" "$i" "${hosts[$i]}"
+    done
+    read -p "Choose which one to connect" id 
+    ssh ${hosts[$((id))]}
+else
+    echo "No host found"
+fi
